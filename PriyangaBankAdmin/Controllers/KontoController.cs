3c355b2e6cbc05a611af23f7bc569e517e1f9e40
Priyanga Kulamithra <priyanga.kulamithra@gmail.com>
2021-05-13 13:43:00 +0200

    Transaction ajax load 20 more

--- PriyangaBankAdmin/Controllers/KontoController.cs
+++ PriyangaBankAdmin/Controllers/KontoController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using PriyangaBankAdmin.Services;
 using PriyangaBankAdmin.ViewModels.Home.Konto;
+using PriyangaBankAdmin.ViewModels.Konto;
 
 namespace PriyangaBankAdmin.Controllers
 {
@@ -30,7 +31,7 @@ namespace PriyangaBankAdmin.Controllers
                 AccountId = accountId,
                 Balance = _dbContext.GetTransactions(accountId).First().Balance
             };
-            viewmodel.Transactions = _dbContext.GetTransactions(accountId).Select(t=>new TransactionItem
+            viewmodel.Transactions = _dbContext.GetTransactions(accountId).Take(20).Select(t=>new TransactionItem
             {
                 TransactionId = t.TransactionId,
                 TransactionDescription = t.Operation,
@@ -38,6 +39,21 @@ namespace PriyangaBankAdmin.Controllers
                 Amount = t.Amount,
                 Balance = t.Balance
             });
+            return View(viewmodel);
+        }
+
+        public IActionResult GetTransactionsFrom(int skip, int accountId)
+        {
+            var viewmodel = new KontoGetTransactionsFromViewModel();
+            viewmodel.Transactions = _dbContext.GetTransactions(accountId).Skip(skip).Take(20).Select(t =>
+                new TransactionItem
+                {
+                    TransactionId = t.TransactionId,
+                    TransactionDescription = t.Operation,
+                    Date = t.Date,
+                    Amount = t.Amount,
+                    Balance = t.Balance
+                });
             return View(viewmodel);
         }
     }
---
+++ PriyangaBankAdmin/ViewModels/Konto/KontoGetTransactionsFromViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PriyangaBankAdmin.ViewModels.Home.Konto;
+
+namespace PriyangaBankAdmin.ViewModels.Konto
+{
+    public class KontoGetTransactionsFromViewModel
+    {
+        public IEnumerable<TransactionItem> Transactions { get; set; }
+
+    }
+}
